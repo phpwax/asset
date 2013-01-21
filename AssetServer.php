07f6d5cf16fcc9d20cf5da19d7fe500728bfe8d4
Ross Riley <riley.ross@gmail.com>
2013-01-21 12:25:20 +0000

    wrong permission check

--- AssetServer.php
+++ AssetServer.php
@@ -15,7 +15,7 @@ class AssetServer {
   static public function symlink_bundle($bundle, $type, $source_path, $target_link = false) {
     if(!$target_link) $target_link = PUBLIC_DIR.$type."/".$bundle;
     if(!is_link($target_link)) {
-      if(is_writable(basename($target_link))) {
+      if(is_writable(dirname($target_link))) {
         symlink($source_path, $target_link);
       } else {
         throw new \Exception("Unable to create $type bundle at $target_link : Allow write access to parent folder");
