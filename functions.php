ee14cf84ed95fbe933a189e5d3fe628c388de0cd
Ross Riley <riley.ross@gmail.com>
2013-01-23 14:18:34 +0000

    oops… logic fail

--- functions.php
+++ functions.php
@@ -8,10 +8,10 @@ use Wax\Asset\AssetServer;
  * @author Ross Riley
  **/
 function css_bundle($name, $options=array(), $plugin="") {
-  echo AssetServer::bundle_builder($name, $options = array(), $plugin="","stylesheets");
+  echo AssetServer::bundle_builder($name, $options, $plugin,"stylesheets");
 }
 
 function js_bundle($name, $options = array(), $plugin="") {
-  echo AssetServer::bundle_builder($name, $options = array(), $plugin="","javascripts");
+  echo AssetServer::bundle_builder($name, $options, $plugin,"javascripts");
 }
 
