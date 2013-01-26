1b66970a26e1935905fd187e02cd9762782fbf90
Ross Riley <riley.ross@gmail.com>
2013-01-26 12:26:55 +0000

    pass in bundle name

--- AssetServer.php
+++ AssetServer.php
@@ -153,8 +153,8 @@ class AssetServer {
       if($plugin) {
         $as = \AutoLoader::get_asset_server();
         if($as->handles($type."/".$name)) {
-          $as->load();
           $asset_bundle = $type."_".$name;
+          $as->load($asset_bundle);
           if($as->asset_manager->has($asset_bundle)) {
             $base = dirname(dirname($as->asset_manager->get($asset_bundle)->getSourceRoot()))."/";
             $d = $as->asset_manager->get($asset_bundle)->getSourceRoot();
