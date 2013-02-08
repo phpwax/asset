ca52b450c0cd8159dbf73ce875aedb284f27f7e0
Ross Riley <riley.ross@gmail.com>
2013-02-08 13:29:26 +0000

    fix to bug on wrong object name

--- AssetServer.php
+++ AssetServer.php
@@ -167,7 +167,7 @@ class AssetServer {
     if($mapped_mime) return $mapped_mime;
     else {
       $finfo = new \finfo(FILEINFO_MIME_TYPE);
-      return $finfo->buffer($asset->dump());
+      return $finfo->buffer($asset_file->dump());
     }
   }
   
