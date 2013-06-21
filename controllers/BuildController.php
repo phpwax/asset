48ff03bd1b56572e864fa20d94b10305c57d7ceb
Sheldon Els <sheldon.els@gmail.com>
2013-06-21 12:29:16 +0100

    non namespaced controllers for now, wax doesn't support them yet

--- controllers/BuildController.php
+++ controllers/BuildController.php
@@ -1,5 +1,4 @@
 <?php
-namespace Wax\Asset;
 class BuildController extends WaxController{
   public function method_missing(){
     print_r("here");exit;
--- controllers/StylesheetsController.php
+++ controllers/StylesheetsController.php
@@ -1,5 +1,4 @@
 <?php
-namespace Wax\Asset;
 class StylesheetsController extends BuildController{
   public function build(){
     return parent::method_missing();
