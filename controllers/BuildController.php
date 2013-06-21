bf4e68759a0e0f5a91a26b1b10b7a56ec9d80d4b
Sheldon Els <sheldon.els@gmail.com>
2013-06-21 16:22:20 +0100

    was missing the media part of wildfire.media

--- controllers/BuildController.php
+++ controllers/BuildController.php
@@ -4,7 +4,9 @@ class BuildController extends WaxController{
   public $use_view = false;
   public function method_missing(){
     $as = AutoLoader::get_asset_server();
+    $filename = array_pop($this->route_array);
+    $filename = substr($filename, 0, strrpos($filename, "."));
     $this->response->add_header("Content-Type", $as->mime($this->action));
-    $this->response->write($as->built_bundle(WaxURL::get("id"), $this->action));
+    $this->response->write($as->built_bundle($filename, $this->action));
   }
 }
